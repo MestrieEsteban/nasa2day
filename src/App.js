d8a3c5722567a45b2dd288904fc64fb6e12755f3
Esteban Mestrie <e.mestrie@isartdigital.com>
2022-03-15 14:22:16 +0100

    V1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import HttpClient from "./HttpClient"
+import './styles/App.scss';
+import moment from 'moment';
 
-function App() {
+
+
+const App = () => {
+  const [apod, setApod] = useState({})
+
+  useEffect(() => {
+    HttpClient.getApodByDay().then(apodData => {
+      setApod(apodData.data)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+		<div className="content" style={{backgroundImage: `url(/images/bg.svg)`}}>
+			<h1 className="title">NASA2DAY</h1>
+			<p className="subtitle">{moment().format("DD/MM/YYYY")}</p>
+			{apod && (
+				<div>
+					<div className="ImageTitle">
+						{apod.title} 
+					</div>
+					<img src={apod.hdurl ? apod.hdurl : apod.hdurl} alt="APOD" className="ImageDay" />
+					<div className="description">
+						<p>{apod.explanation}</p>
+					</div>
+				</div>
+			)}
+		</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
