47748c49374d82d3f2b691b26e5ee7c5aed9c638
Esteban Mestrie <e.mestrie@isartdigital.com>
2022-03-15 14:42:54 +0100

    V1.1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from "react"
 import HttpClient from "./HttpClient"
 import './styles/App.scss';
 import moment from 'moment';
+import random from './randomDate';
 
+const randomDate = random.randomDate(moment().format("YYYY-MM-DD"), "1995-06-16").format("YYYY-MM-DD");
 
 
 const App = () => {
   const [apod, setApod] = useState({})
 
   useEffect(() => {
-    HttpClient.getApodByDay().then(apodData => {
+    HttpClient.getApodByDay(randomDate).then(apodData => {
       setApod(apodData.data)
     })
   }, [])
@@ -18,11 +20,11 @@ const App = () => {
     <div className="app">
 		<div className="content" style={{backgroundImage: `url(/images/bg.svg)`}}>
 			<h1 className="title">NASA2DAY</h1>
-			<p className="subtitle">{moment().format("DD/MM/YYYY")}</p>
+			<p className="subtitle">{randomDate}</p>
 			{apod && (
 				<div>
 					<div className="ImageTitle">
-						{apod.title} 
+						{apod.title}
 					</div>
 					<img src={apod.hdurl ? apod.hdurl : apod.hdurl} alt="APOD" className="ImageDay" />
 					<div className="description">
